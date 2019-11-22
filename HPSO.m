16d284de662741fe54ffa7369bd51cb4a1a68a9d
Nile Walker <niwal7@morgan.edu>
2019-11-22 06:15:56 -0500

    Several Syntax Fixes to HPSO.m

--- HPSO.m
+++ HPSO.m
@@ -18,9 +18,10 @@ zero_particle.Velocity = zeros(1,Dim);
 zero_particle.Cost = inf;
 zero_particle.Best.Position = zeros(1,Dim);
 zero_particle.Best.Cost = inf;
-pop1 = SpawnPopulation(zero_particle,Explosive_Radius,InitialSwarmSize);
+pop1 = SpawnPopulation(zero_particle,Explosive_Radius,InitialSwarmSize,50);
 pop1.C1 = C1;
 pop1.C2 = C2;
+pop1.W = W;
 populationQ = [pop1];
 %% Main Loop
 % parpool('local',2);
@@ -33,9 +34,9 @@ for i=1:epochs
         if populationQ(e).alive 
             %Kill and skip iteration 
             active = false;
-            for n = 1:length(populationQ(e).members)
+            for n = 1:length(populationQ(e).member)
                 % Skip to next particle
-                if populationQ(e).members(n).alive
+                if populationQ(e).member(n).alive
                     active = true;
                     populationQ(e).member(n).Velocity = populationQ(e).W * populationQ(e).member(n).Velocity ...
                         + populationQ(e).C1*rand(1,Dim) .* (populationQ(e).member(n).Best.Position - populationQ(e).member(n).Position)...
@@ -44,17 +45,17 @@ for i=1:epochs
                     populationQ(e).member(n).Position = populationQ(e).member(n).Position + populationQ(e).member(n).Velocity;
                     populationQ(e).member(n).Cost = CostFunction(populationQ(e).member(n).Position);  
 
-                    populationQ(e).History(n).push(populationQ(e).member(n).Cost)
-                    if populationQ(e).History(n).hasConverged
-                        if populationQ(e).InitialCost - populationQ(e).Best > MinimalImprovementThresh
-                            popn = SpawnPopulation(Seed,S_radius,Explosive_Fragments,50);
+                    %populationQ(e).History(n).push(populationQ(e).member(n).Cost)
+                    if rand > .5 %populationQ(e).History(n).hasConverged
+                        if populationQ(e).InitialCost -  populationQ(e).member(n).Cost > MinimalImprovementThresh
+                            popn = SpawnPopulation(populationQ(e).member(n),Explosive_Radius,Explosive_Fragments,50);
                             popn.C1 = C1;
                             % Increase Social Component Per Layer
-                            popn.C2 = popn.C2 + 1;
+                            popn.C2 = populationQ(e).C2 + 1;
                             append(populationQ,popn);
                         end
                         % Remove Particle
-                        populationQ(e).members(n).alive = false; 
+                        populationQ(e).member(n).alive = false; 
                     end
                     if populationQ(e).member(n).Cost < populationQ(e).member(n).Best.Cost    
                         populationQ(e).member(n).Best.Position = populationQ(e).member(n).Position;
@@ -63,7 +64,7 @@ for i=1:epochs
                 end
             end
             populationQ(e).W = populationQ(e).W * D;
-            for n = 1:length(populationQ(e).members)
+            for n = 1:length(populationQ(e).member)
                 if populationQ(e).member(n).alive
                     if populationQ(e).member(n).Best.Cost < populationQ(e).Best.Cost
                         populationQ(e).Best = populationQ(e).member(n).Best;
